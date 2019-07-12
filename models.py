f0d12842c6746c02e6f67794660ab5a10a971b24
light1726 <lu-h17@mails.tsinghua.edu.cn>
2019-07-12 18:31:07 +0800

    some structures

--- models.py
+++ models.py
@@ -3,16 +3,18 @@ from tensorflow import keras
 
 
 class DNNClassifier(object):
-    def __init__(self, out_dims, hiddens, name):
+    def __init__(self, out_dims, hiddens, drop_rate, name):
         self.name = name
         with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
             self.dense_layers = []
             for i, units in enumerate(hiddens):
-                dense = keras.layers.Dense(units, name='dense{}'.format(i))
+                dense = keras.layers.Dense(units, activation=tf.nn.sigmoid,
+                                           name='dense{}'.format(i))
                 self.dense_layers.append(dense)
+            self.dropout_layer = keras.layers.Dropout(rate=drop_rate)
             self.output_dense = keras.layers.Dense(units=out_dims)
 
-    def __call__(self, inputs, labels=None, lengths=None):
+    def __call__(self, inputs, use_dropout=True, labels=None, lengths=None):
         """
         :param inputs: [batch, time, in_dims]
         :param labels: [batch, time, out_dims], should be one-hot
@@ -22,10 +24,13 @@ class DNNClassifier(object):
         cur_layer = inputs
         for layer in self.dense_layers:
             cur_layer = layer(cur_layer)
+            cur_layer = tf.cond(use_dropout,
+                                lambda: self.dropout_layer(cur_layer),
+                                lambda: cur_layer)
         logits = self.output_dense(cur_layer)  # [batch, time, out_dims]
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
             labels=tf.stop_gradient(labels),
-            logits=logits,
-            axis=-1) if labels is not None else None
+            logits=logits) if labels is not None else None
+        cross_entropy = tf.reduce_mean(cross_entropy)
         return {'logits': logits,  # [time, dims]
                 'cross_entropy': cross_entropy}
