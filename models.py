6380b39d74b67e5122dca1b64bf653cd3d580789
light1726 <lu-h17@mails.tsinghua.edu.cn>
2019-07-13 17:29:53 +0800

    add CNN_DNN model definition

--- models.py
+++ models.py
@@ -24,13 +24,66 @@ class DNNClassifier(object):
         cur_layer = inputs
         for layer in self.dense_layers:
             cur_layer = layer(cur_layer)
-            cur_layer = tf.cond(use_dropout,
+            cur_layer = tf.cond(tf.constant(use_dropout, dtype=tf.bool),
                                 lambda: self.dropout_layer(cur_layer),
                                 lambda: cur_layer)
         logits = self.output_dense(cur_layer)  # [batch, time, out_dims]
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
             labels=tf.stop_gradient(labels),
             logits=logits) if labels is not None else None
-        cross_entropy = tf.reduce_mean(cross_entropy)
+        mask = tf.sequence_mask(lengths, dtype=tf.float32) if lengths is not None else 1.0
+        cross_entropy *= mask
+        cross_entropy = tf.reduce_mean(cross_entropy) if cross_entropy is not None else None
+        return {'logits': logits,  # [time, dims]
+                'cross_entropy': cross_entropy}
+
+
+class CnnDnnClassifier(object):
+    def __init__(self, out_dims, n_cnn, cnn_hidden, dense_hiddens,
+                 name='cnn_dnn_classifier'):
+        self.name = name
+        with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
+            with tf.variable_scope('multi_scale_cnn'):
+                self.conv_stack = []
+                for i in range(n_cnn):
+                    cnn_layer = keras.layers.Conv1D(filters=cnn_hidden,
+                                                    kernel_size=i + 1,
+                                                    padding='same',
+                                                    name='cnn{}'.format(i + 1))
+                    self.conv_stack.append(cnn_layer)
+            self.pooling_layer = keras.layers.MaxPool1D(pool_size=2, strides=1,
+                                                        padding='same')
+            with tf.variable_scope('dnn_layers'):
+                self.dense_layers = []
+                for i, hidden in enumerate(dense_hiddens):
+                    dense_layer = keras.layers.Dense(units=hidden,
+                                                     activation=tf.nn.tanh,
+                                                     name='dense{}'.format(i))
+                    self.dense_layers.append(dense_layer)
+            self.output_layer = keras.layers.Dense(units=out_dims, activation=None,
+                                                   name='output_dense')
+
+    def __call__(self, inputs, labels=None, lengths=None):
+        # 1. conv1d stack
+        cnn_stack_outs = tf.concat([conv_layer(inputs) for conv_layer in self.conv_stack],
+                                   axis=-1)
+        # 2. maxpooling over time
+        maxpool_outs = self.pooling_layer(cnn_stack_outs)
+
+        # 3. DNN stack
+        dnn_outs = maxpool_outs
+        for dense in self.dense_layers:
+            dnn_outs = dense(dnn_outs)
+
+        # 4. output layer
+        logits = self.output_layer(dnn_outs)
+
+        # compute loss
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+            labels=tf.stop_gradient(labels),
+            logits=logits) if labels is not None else None
+        mask = tf.sequence_mask(lengths, dtype=tf.float32) if lengths is not None else 1.0
+        cross_entropy *= mask
+        cross_entropy = tf.reduce_mean(cross_entropy) if cross_entropy is not None else None
         return {'logits': logits,  # [time, dims]
                 'cross_entropy': cross_entropy}
